ffa44a0237bae64cab8fe7099dc85baeaa5c471b
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-06-20 23:16:33 +0100

    Add configurable API token auth

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'exampleapi',
 ]
 
@@ -120,3 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+default_permission_class = (
+    'rest_framework.permissions.IsAuthenticated' if os.environ.get('ENABLE_AUTHENTICATION', False) else
+    'rest_framework.permissions.AllowAny'
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        default_permission_class,
+    ),
+}
