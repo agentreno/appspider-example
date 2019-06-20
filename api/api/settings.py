107503ec6999719980e2947caab1433acf7a00c2
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-06-20 19:40:05 +0100

    Fix allowed hosts for Heroku

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-%r-sz9octbmzxcy1eublof)*$dnta3f=m=xbrhn__h$_0i#1o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['appspider-example.herokuapp.com']
 
 
 # Application definition
